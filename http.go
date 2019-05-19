caed150d9826c2ae23fb944143bae7a19ee0ce7a
wujunze <itwujunze@163.com>
2019-05-19 14:13:11 +0800

    add interface

---
+++ http.go
@@ -0,0 +1,10 @@
+package http
+
+import (
+	"net/url"
+)
+
+type Client interface {
+	Get(uri string, params map[string]string) ([]byte, error)
+	Post(uri string, params url.Values) (string,error)
+}
